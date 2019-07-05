0335038aaebc2fb2193f477a9ba5f76d60871275
new4 <new4wang@163.com>
2019-07-05 19:13:04 +0800

    data of axios response

--- scripts/check.js
+++ scripts/check.js
@@ -1,13 +1,16 @@
 const axios = require('axios');
 const cheerio = require('cheerio');
+
 axios
   .get('https://cn.eslint.org/docs/rules/')
-  .then((response) => {
-    const $ = cheerio.load(response, {
+  .then(({ data }) => {
+    const $ = cheerio.load(data, {
       ignoreWhitespace: true,
     });
-    const out = $.html();
-    console.log(out);
+
+    console.log($('main').html());
+    console.log($('main').find('h2').length);
+    console.log($('#possible-errors').text());
   })
   .catch((error) => {
     console.log(error);
