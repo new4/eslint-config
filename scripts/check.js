6abf6c07f79965668fabf09e06a106aa8adcf7d2
new4 <new4wang@163.com>
2019-07-08 13:48:19 +0800

    filter 官网和本项目的 rules

--- scripts/check.js
+++ scripts/check.js
@@ -1,17 +1,37 @@
 const axios = require('axios');
 const cheerio = require('cheerio');
 
+const {
+  fileOp: {
+    getExistFiles,
+  },
+  underPath,
+} = require('@new4/utils');
+
+const RULES_DIR = underPath('root', 'rules');
+
 axios
   .get('https://cn.eslint.org/docs/rules/')
   .then(({ data }) => {
-    const $ = cheerio.load(data, {
-      ignoreWhitespace: true,
-    });
+    const $ = cheerio.load(data);
+    const titles = $('main')
+      .find('h2')
+      .map((index, ele) => $(ele)
+        .text()
+        .replace(/[.\s]/g, '')
+        .toLowerCase())
+      .toArray()
+      .filter(title => title !== 'deprecated' && title !== 'removed')
+      .sort();
 
-    console.log($('main').html());
-    console.log($('main').find('h2').length);
-    console.log($('#possible-errors').text());
-  })
-  .catch((error) => {
-    console.log(error);
+    console.log(titles);
+
+    const ruleFiles = getExistFiles(RULES_DIR)
+      .map(file => file
+        .replace(/\.js$/, '')
+        .replace(/[-]/g, ''))
+      .filter(file => file !== '_eslintrestrictedglobals')
+      .sort();
+
+    console.log(ruleFiles);
   });
